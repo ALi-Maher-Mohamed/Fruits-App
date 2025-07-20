ef835a8c81fcdd0e00b6745959fc3d08804ddc41
AliMaher <ali.maher0013@gmail.com>
2025-07-21 00:59:06 +0300

    add ptoducts List View

---
+++ lib/models/item_model.dart
@@ -0,0 +1,15 @@
+class ItemModel {
+  final String name;
+  final String image;
+  final double price;
+  final double rating;
+  final int numberOfRatings;
+
+  ItemModel({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.rating,
+    required this.numberOfRatings,
+  });
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_app/models/category_model.dart';
+import 'package:fruits_app/models/item_model.dart';
 import 'package:fruits_app/utils/custom_colors.dart';
 
 class HomeView extends StatelessWidget {
@@ -34,6 +35,29 @@ class HomeView extends StatelessWidget {
       image: 'assets/images/category/vegetables.png',
     ),
   ];
+  final List<ItemModel> items = [
+    ItemModel(
+      numberOfRatings: 207,
+      name: 'Banana',
+      image: 'assets/images/fruits/banana.png',
+      rating: 4.5,
+      price: 20,
+    ),
+    ItemModel(
+      numberOfRatings: 207,
+      name: 'pappers',
+      image: 'assets/images/fruits/pappers.png',
+      rating: 4.5,
+      price: 20,
+    ),
+    ItemModel(
+      numberOfRatings: 207,
+      name: 'Oranges',
+      image: 'assets/images/fruits/oranges.png',
+      rating: 4.5,
+      price: 20,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,26 +72,105 @@ class HomeView extends StatelessWidget {
             SizedBox(height: 17),
             categoryListView(),
             SizedBox(height: 25),
-            Row(
-              children: [
-                Text(
-                  'Fruits',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Spacer(),
-                Text(
-                  'See All',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: CustomColors.primaryColor,
-                  ),
-                ),
-              ],
-            ),
+            fruitsAndSeeAll(),
+            SizedBox(height: 25),
+            productsListView(),
+            SizedBox(height: 25),
           ],
         ),
       ),
+    );
+  }
+
+  Expanded productsListView() {
+    return Expanded(
+      child: ListView.separated(
+        separatorBuilder: (context, index) => SizedBox(width: 10),
+        scrollDirection: Axis.horizontal,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 120,
+                width: 147,
+                decoration: BoxDecoration(
+                  color: CustomColors.neutralColor,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0,
+                        vertical: 7,
+                      ),
+                      child: Image.asset(items[index].image),
+                    ),
+                    Positioned(
+                      bottom: 5,
+                      right: 10,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          child: Icon(Icons.add, size: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                items[index].name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Icon(Icons.star, color: Colors.yellow),
+                  Text(
+                    '${items[index].rating} (${items[index].numberOfRatings})',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Text(
+                "\$ ${items[index].price}",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Row fruitsAndSeeAll() {
+    return Row(
+      children: [
+        Text(
+          'Fruits',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Spacer(),
+        Text(
+          'See All',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: CustomColors.primaryColor,
+          ),
+        ),
+      ],
     );
   }
 
